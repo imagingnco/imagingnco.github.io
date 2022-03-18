ecca994a06a749f5b3ec083d9fd8e982c4f1cd75
chriamue <chriamue@users.noreply.github.com>
2022-03-18 08:13:15 +0000

    deploy: imagingnco/imagingnco-web@7e0782db79c0ad541c05b018971507614ce5a9c7

---
+++ types/components/HowToConvert.vue.d.ts
@@ -0,0 +1,7 @@
+declare const _default: {
+    components: {
+        HowToTile: any;
+    };
+};
+export default _default;
+//# sourceMappingURL=HowToConvert.vue.d.ts.map
---
+++ types/components/HowToTile.vue.d.ts
@@ -0,0 +1,9 @@
+declare const _default: {
+    props: {
+        text: {
+            type: StringConstructor[];
+        };
+    };
+};
+export default _default;
+//# sourceMappingURL=HowToTile.vue.d.ts.map
