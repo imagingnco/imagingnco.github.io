07c4211a3818d1eac0ee3949085269dc5fb9a01e
chriamue <chriamue@users.noreply.github.com>
2022-03-25 18:31:00 +0000

    deploy: imagingnco/imagingnco-web@a263a5449714cb604a28d24880f2aa11e0ce5521

---
+++ types/components/PluginTitle.vue.d.ts
@@ -0,0 +1,9 @@
+declare const _default: {
+    props: {
+        title: {
+            type: StringConstructor[];
+        };
+    };
+};
+export default _default;
+//# sourceMappingURL=PluginTitle.vue.d.ts.map
