cba1eb42e37aafa4ea408a90d680f3d9849f7379
chriamue <chriamue@users.noreply.github.com>
2022-03-25 19:39:07 +0000

    deploy: imagingnco/imagingnco-web@479774990c496dd8c04e5fe0b33d6128cb11583e

---
+++ types/plugins/Rotate.vue.d.ts
@@ -0,0 +1,17 @@
+import "vue-slider-component/theme/antd.css";
+export interface Configs {
+    [key: string]: any;
+}
+declare interface PluginConfigData {
+    options: any;
+    selected: String;
+}
+declare const _default: import("vue").DefineComponent<{
+    defaultConfig: ObjectConstructor;
+}, unknown, PluginConfigData, {}, {
+    updateConfig(): Promise<void>;
+}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, Record<string, any>, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+    defaultConfig: ObjectConstructor;
+}>>, {}>;
+export default _default;
+//# sourceMappingURL=Rotate.vue.d.ts.map
