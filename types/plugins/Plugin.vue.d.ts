3065e4fdd8707a98066f838c2e96352e989af713
chriamue <chriamue@users.noreply.github.com>
2022-03-25 11:06:38 +0000

    deploy: imagingnco/imagingnco-web@2043ba3c535d6e3b5924bdee0174af2dbd55e1f4

---
+++ types/plugins/Plugin.vue.d.ts
@@ -0,0 +1,30 @@
+import { Ref } from "vue";
+import { PluginModule } from "../models/plugin_module";
+import { Config } from "../models/config";
+declare interface PluginData {
+    ui: Ref;
+    icon: Ref;
+    processor: any;
+    enabled: boolean;
+    keyCounter: number;
+    pluginKey: string;
+    config: Object;
+}
+declare const _default: import("vue").DefineComponent<{
+    name: StringConstructor;
+    plugin: () => PluginModule;
+    url: StringConstructor;
+    defaultConfig: () => Config[];
+}, unknown, PluginData, {}, {
+    loadDefaultConfig(): Promise<void>;
+    loadUI(): Promise<void>;
+    updateConfig(config: Object): Promise<void>;
+    updateEnabled(enabled: boolean): Promise<void>;
+}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, Record<string, any>, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+    name: StringConstructor;
+    plugin: () => PluginModule;
+    url: StringConstructor;
+    defaultConfig: () => Config[];
+}>>, {}>;
+export default _default;
+//# sourceMappingURL=Plugin.vue.d.ts.map
