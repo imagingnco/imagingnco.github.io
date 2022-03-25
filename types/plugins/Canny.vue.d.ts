6abdfcd6a4fcdf8a7b8f500e8ed7ecde22f883d8
chriamue <chriamue@users.noreply.github.com>
2022-03-25 19:16:40 +0000

    deploy: imagingnco/imagingnco-web@6b377adc7d5efb3e3652a3902ecbe09fa660e1bb

---
+++ types/plugins/Canny.vue.d.ts
@@ -0,0 +1,20 @@
+import "vue-slider-component/theme/antd.css";
+export interface Configs {
+    [key: string]: any;
+}
+interface Config {
+    key: string;
+    value: any;
+}
+declare interface PluginConfigData {
+    config: Config[];
+}
+declare const _default: import("vue").DefineComponent<{
+    defaultConfig: ObjectConstructor;
+}, unknown, PluginConfigData, {}, {
+    updateConfig(): Promise<void>;
+}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, Record<string, any>, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+    defaultConfig: ObjectConstructor;
+}>>, {}>;
+export default _default;
+//# sourceMappingURL=Canny.vue.d.ts.map
