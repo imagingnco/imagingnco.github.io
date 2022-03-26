7224f23b7f3d70646b198a5b680ddf0680eb11fa
chriamue <chriamue@users.noreply.github.com>
2022-03-26 09:48:42 +0000

    deploy: imagingnco/imagingnco-web@488f1132694403b5f72ab1492d8bb90607350b06

---
+++ types/components/DownloadButton.vue.d.ts
@@ -0,0 +1,25 @@
+declare const _default: import("vue").DefineComponent<{
+    onClick: {
+        type: FunctionConstructor;
+        required: true;
+    };
+    filetype: {
+        type: StringConstructor;
+        required: true;
+    };
+}, {
+    filetype: import("vue").WritableComputedRef<string>;
+}, any, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, "update:filetype"[], "update:filetype", import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+    onClick: {
+        type: FunctionConstructor;
+        required: true;
+    };
+    filetype: {
+        type: StringConstructor;
+        required: true;
+    };
+}>> & {
+    "onUpdate:filetype"?: ((...args: any[]) => any) | undefined;
+}, {}>;
+export default _default;
+//# sourceMappingURL=DownloadButton.vue.d.ts.map
